940b8ed2fdec9ea907ac36675b5bf4531f26c9c3
mackee <macopy123@gmail.com>
2015-10-26 15:26:22 +0900

    add feature: bulk send by multiple session key in header

--- proxy.go
+++ proxy.go
@@ -1,7 +1,11 @@
 package kuiperbelt
 
 import (
+	"bytes"
+	"encoding/json"
+	"gopkg.in/pp.v1"
 	"io"
+	"log"
 	"net/http"
 )
 
@@ -14,32 +18,70 @@ func (p *Proxy) Register() {
 }
 
 func (p *Proxy) HandlerFunc(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	pp.Println(r)
+
 	if r.Method != "POST" {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		io.WriteString(w, "Required POST method.")
 		return
 	}
-	key := r.Header.Get(p.Config.SessionHeader)
-	s, err := GetSession(key)
-	if err == sessionNotFoundError {
-		w.WriteHeader(http.StatusNotFound)
+	keys, ok := r.Header[p.Config.SessionHeader]
+	if !ok || len(keys) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
 		io.WriteString(w, "Session is not found.")
 		return
 	}
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		io.WriteString(w, err.Error())
+	ss := make([]Session, 0, len(keys))
+	se := make([]sessionError, 0, len(keys))
+	for _, key := range keys {
+		s, err := GetSession(key)
+		if err != nil {
+			se = append(se, sessionError{err.Error(), key})
+		}
+		ss = append(ss, s)
+	}
+	if len(se) > 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Header().Add("Content-Type", "application/json")
+		enc := json.NewEncoder(w)
+		enc.Encode(struct {
+			Errors []sessionError `json:"errors"`
+		}{
+			Errors: se,
+		})
 		return
 	}
 
-	_, err = io.Copy(s, r.Body)
-	defer r.Body.Close()
+	b := new(bytes.Buffer)
+	_, err := io.Copy(b, r.Body)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		io.WriteString(w, err.Error())
-		return
+	}
+	bs := b.Bytes()
+
+	for _, s := range ss {
+		go p.sendMessage(s, bs)
 	}
 
 	w.WriteHeader(http.StatusOK)
-	io.WriteString(w, "OK")
+	io.WriteString(w, `{"result":"OK"}`)
+}
+
+type sessionError struct {
+	Error   string `json:"error"`
+	Session string `json:"session"`
+}
+
+func (p *Proxy) sendMessage(s Session, bs []byte) {
+	nw, err := s.Write(bs)
+	if err != nil {
+		log.Printf("[ERROR] write to session error: session=%s, error=%s\n", s.Key(), err)
+		return
+	}
+	if nw != len(bs) {
+		log.Printf(
+			"[ERROR] write to session is short: session=%s, write_byte=%d, return_byte=%d\n",
+			s.Key(), len(bs), nw,
+		)
+	}
 }
